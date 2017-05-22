32eade19dfec5314f4cdbdef97b6550f3b388f72
Maksim Zubkov <maksim.v.zubkov@gmail.com>
2017-05-22 17:18:57 +0300

    Adaptation for python 3

--- triangles.py
+++ triangles.py
@@ -2,7 +2,7 @@ from OpenGL.GLUT import *
 from OpenGL.GLU import *
 from OpenGL.GL import *
 from OpenGL.raw.GL.ARB.vertex_array_object import glGenVertexArrays, \
-                                                  glBindVertexArray
+                          glBindVertexArray
 import numpy as np
 
 import sys
@@ -11,81 +11,81 @@ NumVertices = 6
 VAOs = GLuint(0)
 
 def ReadShader(filename):
-	with open(filename, 'rb') as file:
-		return file.read()
-	return ''
+    with open(filename, 'rb') as file:
+        return file.read()
+    return ''
 
 def LoadShaders(shaders):
-	program = glCreateProgram()
+    program = glCreateProgram()
 
-	for shaderEntry in shaders:
-		shader = glCreateShader(shaderEntry[0])
-		source = ReadShader(shaderEntry[1])
+    for shaderEntry in shaders:
+        shader = glCreateShader(shaderEntry[0])
+        source = ReadShader(shaderEntry[1])
 
-		glShaderSource(shader, source)
-		glCompileShader(shader)
+        glShaderSource(shader, source)
+        glCompileShader(shader)
 
-		log = glGetShaderInfoLog(shader)
-		if log:
-			print "Error in shader: %s" % (shaderEntry[1])
-			print log
+        log = glGetShaderInfoLog(shader)
+        if log:
+            print("Error in shader: {0}".format(shaderEntry[1]))
+            print(log)
 
-		glAttachShader(program, shader)
+        glAttachShader(program, shader)
 
-	glLinkProgram(program)
+    glLinkProgram(program)
 
-	return program
+    return program
 
 def init():
-	global VAOs
+    global VAOs
 
-	glGenVertexArrays(1, VAOs)
-	glBindVertexArray(VAOs)
+    glGenVertexArrays(1, VAOs)
+    glBindVertexArray(VAOs)
 
-	vertices = [
-		[-0.90, -0.90],
-		[0.85, -0.90],
-		[-0.90, 0.85],
-		[0.90, -0.85],
-		[0.90, 0.90],
-		[-0.85, 0.90],
-	]
+    vertices = [
+        [-0.90, -0.90],
+        [ 0.85, -0.90],
+        [-0.90,  0.85],
+        [ 0.90, -0.85],
+        [ 0.90,  0.90],
+        [-0.85,  0.90],
+    ]
 
-	vertices = np.array(vertices, dtype="f")
+    vertices = np.array(vertices, dtype="f")
 
-	buffers = glGenBuffers(1)
-	glBindBuffer(GL_ARRAY_BUFFER, buffers)
-	glBufferData(GL_ARRAY_BUFFER, vertices, GL_STATIC_DRAW)
+    buffers = glGenBuffers(1)
+    glBindBuffer(GL_ARRAY_BUFFER, buffers)
+    glBufferData(GL_ARRAY_BUFFER, vertices, GL_STATIC_DRAW)
 
-	shaders = [
-		(GL_VERTEX_SHADER, "triangles.vert"),
-		(GL_FRAGMENT_SHADER, "triangles.frag"),
-	]
+    shaders = [
+        (GL_VERTEX_SHADER, "triangles.vert"),
+        (GL_FRAGMENT_SHADER, "triangles.frag"),
+    ]
 
-	program = LoadShaders(shaders)
-	glUseProgram(program)
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, None)
-	
-	glEnableVertexAttribArray(0)
+    program = LoadShaders(shaders)
+    glUseProgram(program)
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, None)
+    
+    glEnableVertexAttribArray(0)
 
 def display():
-	glClear(GL_COLOR_BUFFER_BIT)
+    glClear(GL_COLOR_BUFFER_BIT)
 
-	glBindVertexArray(VAOs);
-	glDrawArrays(GL_TRIANGLES, 0, NumVertices)
+    glBindVertexArray(VAOs);
+    glDrawArrays(GL_TRIANGLES, 0, NumVertices)
 
-	glFlush()
+    glFlush()
 
 if __name__ == '__main__':
-	glutInit(sys.argv)
-	glutInitDisplayMode(GLUT_RGBA)
-	glutInitWindowSize(512, 512)
-	glutInitContextVersion(3, 3)
-	glutInitContextProfile(GLUT_CORE_PROFILE)
-	glutCreateWindow(sys.argv[0])
+    glutInit(sys.argv)
+    glutInitDisplayMode(GLUT_RGBA)
+    glutInitWindowSize(512, 512)
+    glutInitContextVersion(3, 3)
+    glutInitContextProfile(GLUT_CORE_PROFILE)
+    glutCreateWindow(sys.argv[0])
 
-	init()
-	
-	glutDisplayFunc(display)
-	glutMainLoop()
+    init()
+    
+    glutDisplayFunc(display)
+    glutMainLoop()
 
